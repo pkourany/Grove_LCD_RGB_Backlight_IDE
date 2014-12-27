447c00df0cc05bbd35aee04aa52e1072cdb3e4fc
unknown <pkourany@gmail.com>
2014-12-26 20:55:10 -0500

    First commit

---
+++ firmware/examples/TextDirection/TextDirection.ino
@@ -0,0 +1,79 @@
+/*
+  TextDirection.ino
+  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
+
+  Author:Loovee
+  2013-9-18
+
+  !!! Adapted for Spark Core by Paul Kourany, Dec 26, 2014 !!!
+
+  Grove - Serial LCD RGB Backlight demo.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#if defined (SPARK)
+// Nothing to include if Spark
+#else
+#include <Wire.h>
+#endif
+
+#include "Grove_LCD_RGB_Backlight.h"
+
+rgb_lcd lcd;
+
+int thisChar = 'a';
+
+void setup() 
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    // turn on the cursor:
+    lcd.cursor();
+}
+
+void loop() 
+{
+    // reverse directions at 'm':
+    if (thisChar == 'm') 
+    {
+        // go right for the next letter
+        lcd.rightToLeft();
+    }
+    // reverse again at 's':
+    if (thisChar == 's') 
+    {
+        // go left for the next letter
+        lcd.leftToRight();
+    }
+    // reset at 'z':
+    if (thisChar > 'z')
+    {
+        // go to (0,0):
+        lcd.home();
+        // start again at 0
+        thisChar = 'a';
+    }
+    // print the character
+    lcd.write(thisChar);
+    // wait a second:
+    delay(1000);
+    // increment the letter:
+    thisChar++;
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
